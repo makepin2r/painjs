a22acd0cc2d2392971259fa2065d27d3309611ef
makepin2r <makepin2r@gmail.com>
2021-05-13 22:56:10 +0900

    저장 기능

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+// before setting ctx.fillStyle, 기본 배경색 입히기
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
@@ -71,6 +76,18 @@ function handleCanvasClick(){
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
+    
+function handleCM(event){
+    event.preventDefault();
+}
+
+function saveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "download_paintJS";
+    link.click();
+}
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
@@ -78,6 +95,9 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+
+    // 캔버스 마우스 우클릭 메뉴 방지
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -88,4 +108,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", saveClick);
 }
