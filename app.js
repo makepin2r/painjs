5172484fa54f2f2179f15c68f5b583cb45ed6715
makepin2r <makepin2r@gmail.com>
2021-05-13 21:56:03 +0900

    선 그리기 로직

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,20 +13,34 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
 
-    console.log(x, y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(){
     painting = true;
 }
 
+function onMouseUp(){
+    stopPainting(); // painting logic을 한 곳에 모아두기 위해서
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
