2db9bcd7ce5e27cc768c85a9b7675c603f21b4ea
makepin2r <makepin2r@gmail.com>
2021-05-13 22:36:40 +0900

    fill에서 선 긋기 보이지 않도록 분기 추가

--- app.js
+++ app.js
@@ -29,12 +29,14 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
 
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+    if(!filling){
+        if(!painting){
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
     }
 }
 
